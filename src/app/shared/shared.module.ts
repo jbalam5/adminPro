ce4d625253ed5d18e35f65a0b3f920cc6f801f93
Jbalam <jbalam@mexicovacationsgroup.com>
2018-04-28 12:13:13 -0500

    Se agrega modulo de paginas

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from "@angular/core";
+import { BreadcrumbsComponent } from "./breadcrumbs/breadcrumbs.component";
+import { NopagefoundComponent } from "./nopagefound/nopagefound.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { HeaderComponent } from "./header/header.component";
+
+@NgModule({
+    declarations: [
+        BreadcrumbsComponent,
+        NopagefoundComponent,
+        SidebarComponent,
+        HeaderComponent
+    ],
+    exports: [
+        BreadcrumbsComponent,
+        NopagefoundComponent,
+        SidebarComponent,
+        HeaderComponent
+    ]
+})
+export class SharedModule{
+
+}
